7a8f41d731b5ed564cbed96f94a44b9b7d9d0a14
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-08 12:00:10 -0700

    Work in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 function pick(source, keys) {
+  // console.log(keys.length);
   const result = {};
   for (let i = 0; i < keys.length; i++) {
-    if (keys[i] === source[keys]) {
-      result[keys[i]] = source[keys];
+    if (keys[i] === source[i]) {
+      result[keys[i]] = source[keys[i]];
     }
   }
   return result;
