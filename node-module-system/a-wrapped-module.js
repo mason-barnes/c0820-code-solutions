af97e87a6f6e196ac1e26075786a7d558e2d887f
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-08 10:48:52 -0700

    Completed node module system exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+console.log(typeof exports);
+console.log(exports);
+console.log(typeof require);
+console.log(require);
+console.log(typeof module);
+console.log(module);
+console.log(typeof __filename);
+console.log(__filename);
+console.log(typeof __dirname);
+console.log(__dirname);
