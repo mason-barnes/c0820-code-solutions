597e59b9db69474497a2498f8d617d177723875e
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-09 09:34:32 -0700

    Work in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function omit(source, keys) {
+  const result = {};
+  for (let i = 0; i < keys.length; i++) {
+    console.log(keys.length);
+    if (source[keys[i]] !== keys[i]) {
+      result[keys[i]] = source[keys[i]];
+    }
+  }
+  return result;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars */
 function pick(source, keys) {
-  const object = Object.keys(source);
   const result = {};
-  console.log(object);
   for (let i = 0; i < keys.length; i++) {
-    if (keys[i] === object[i]) {
+    if (source[keys[i]] !== undefined) {
       result[keys[i]] = source[keys[i]];
     }
   }
