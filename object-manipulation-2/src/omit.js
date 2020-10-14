ff290d910bd7443ffbb3801f218b33365a76a7e5
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-14 11:09:27 -0700

    Work in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-unused-vars */
 function omit(source, keys) {
   const result = {};
+  console.log('source', source);
+  console.log('keys', keys);
   for (let i = 0; i < keys.length; i++) {
-    if (keys[i] !== result[i]) {
-      result[source[i]];
+    if (source[keys[i]] !== keys[i]) {
+      result[source[i]] = source[i];
     }
   }
+  console.log(result);
   return result;
 }
