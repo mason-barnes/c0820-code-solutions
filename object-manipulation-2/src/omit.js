0b4ed6791d7b201c345fb2fc521a7dd5e1e75664
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-18 16:42:23 -0700

    Work in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
 function omit(source, keys) {
   const result = {};
-  for (let i = 0; i < keys.length; i++) {
-    if ("The key name matches any of the source names") {
-      remove that source
+  for (const prop in source) {
+    console.log(prop, keys);
+    for (let i = 0; i < keys.length; i++) {
+      if (prop !== keys[i]) {
+        result[prop] = source[prop];
+      }
     }
   }
   return result;
