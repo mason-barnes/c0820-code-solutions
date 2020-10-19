8ae736493a8d83bec8a04f2c9f76c6918578cd98
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-19 10:04:41 -0700

    Work in progress

---
+++ array-manipulation-3/src/equal.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+function equal(first, second) {
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ array-manipulation-3/src/flatten.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+function flatten(array) {
+  const result = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (Array.isArray(array[i])) {
+  //     const section = array[i];
+  //     for (let j = 0; j < section.length; i++) {
+  //       result.push(section[j]);
+  //     }
+  //   }
+  //   result.push(array[i]);
+  // }
+  return result;
+}
