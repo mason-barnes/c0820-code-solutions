973d578bb4efb4497bd72796407bc4f2aa95c9ed
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-20 12:02:24 -0700

    Work in progress

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function unique(array) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const index = array[i];
+    for (let j = 0; j < result.length; j++) {
+      if (index !== result[j]) {
+        result.push(index);
+      }
+    }
+  }
+  return result;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,4 +1,14 @@
 /* eslint-disable no-unused-vars */
 function zip(first, second) {
-
+  const result = [];
+  let length = first.length;
+  if (length > second.length) {
+    length = second.length;
+  }
+  for (let i = 0; i < length; i++) {
+    const array = [];
+    array.push(first[i], second[i]);
+    result.push(array);
+  }
+  return result;
 }
