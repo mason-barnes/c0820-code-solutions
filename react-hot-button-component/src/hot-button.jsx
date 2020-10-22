eabbe449156ee14f591e85a8f686c7326b6522be
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-22 13:31:10 -0700

    Completed react hot button component exercise

---
+++ react-hot-button-component/src/hot-button.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class HotButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickCount: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ clickCount: this.state.clickCount + 1 });
+    // eslint-disable-next-line no-console
+    console.log('clicked', this.state.clickCount);
+  }
+
+  render() {
+    if (this.state.clickCount <= 2) {
+      return <button onClick={this.handleClick} className="brown">Hot Button</button>;
+    } else if (this.state.clickCount >= 3 && this.state.clickCount <= 5) {
+      return <button onClick={this.handleClick} className="purple">Hot Button</button>;
+    } else if (this.state.clickCount >= 6 && this.state.clickCount <= 8) {
+      return <button onClick={this.handleClick} className="light-purple">Hot Button</button>;
+    } else if (this.state.clickCount >= 9 && this.state.clickCount <= 11) {
+      return <button onClick={this.handleClick} className="pink">Hot Button</button>;
+    } else if (this.state.clickCount >= 12 && this.state.clickCount <= 14) {
+      return <button onClick={this.handleClick} className="orange">Hot Button</button>;
+    } else if (this.state.clickCount >= 15 && this.state.clickCount <= 17) {
+      return <button onClick={this.handleClick} className="yellow">Hot Button</button>;
+    } else if (this.state.clickCount >= 15) {
+      return <button onClick={this.handleClick} className="white">Hot Button</button>;
+    }
+  }
+}
