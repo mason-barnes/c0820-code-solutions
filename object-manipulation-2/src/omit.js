8f8f9a585aa356e9968939eb53e622b8b0e632cc
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-26 12:27:13 -0700

    Work in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -4,7 +4,7 @@ function omit(source, keys) {
   for (const prop in source) {
     console.log(prop, keys);
     for (let i = 0; i < keys.length; i++) {
-      if (prop !== keys[i]) {
+      if (keys[i] !== undefined) {
         result[prop] = source[prop];
       }
     }
