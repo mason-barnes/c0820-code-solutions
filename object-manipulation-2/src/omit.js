4ea026519c7cb0ac105f5f15b0952c673be35868
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-27 13:28:14 -0700

    Removed extra loop in code

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,10 +2,8 @@
 function omit(source, keys) {
   const result = {};
   for (const prop in source) {
-    for (let i = 0; i < keys.length; i++) {
-      if (!keys.includes(prop)) {
-        result[prop] = source[prop];
-      }
+    if (!keys.includes(prop)) {
+      result[prop] = source[prop];
     }
   }
   return result;
