e8265c8151396619eda88aad405117d99bc79957
Mason Barnes <hypnosismcb@yahoo.com>
2020-10-28 09:27:45 -0700

    Work in progress

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+function difference(first, second) {
+  const result = [];
+  let longestArray = first;
+  if (longestArray.length < second.length) {
+    longestArray = second;
+  }
+  for (let i = 0; i < longestArray.length; i++) {
+    if (!first.includes(second[i])) {
+      result.push(first[i]);
+    }
+  }
+  return result;
+}
